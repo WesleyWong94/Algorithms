96ccde81d669b7838741416fb664c67ab1779c3c
WesleyWong94 <63189469+WesleyWong94@users.noreply.github.com>
2020-04-05 20:57:21 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace Sort
+{
+    class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            StreamReader reader = new StreamReader(File.OpenRead(@"unsorted_numbers.csv"));
+
+            while (!reader.EndOfStream)
+            {
+                myList.Add(Convert.ToInt32(reader.ReadLine()));
+            }
+
+            Console.WriteLine("Import Success");
+
+
+            PrintArray(myList);
+            Console.WriteLine("\nOriginal Unsorted Array Displayed");
+
+            Console.ReadLine();
+
+            InsertSort(myList);
+            PrintArray(myList);
+            Console.WriteLine("\nInsertion Sorted Array Displayed");
+
+            Console.ReadLine();
+
+            ShellSort(myList);
+            PrintArray(myList);
+            Console.WriteLine("\nShell Sorted Array Displayed");
+
+            Console.ReadLine();
+
+            LinearSearch(myList);
+            Console.WriteLine("\nLinear Search Results");
+
+            Console.ReadLine();
+
+            BinarySearch(myList);
+            Console.WriteLine("\nBinary Search Results");
+
+
+        }
+
+
+        public static void InsertSort(List<int> arr)
+        {
+            int n = arr.Count;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+        }
+
+        static void PrintArray(List<int> arr)
+        {
+            int n = arr.Count;
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+        }
+        public static void ShellSort(List<int> arr)
+        {
+            int n = arr.Count;
+
+            for (int gap = n / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < n; i += 1)
+                {
+                    int temp = arr[i];
+
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                        arr[j] = arr[j - gap];
+
+                    arr[j] = temp;
+                }
+            }
+        }
+        public static int LinearSearch(List<int> arr)
+        {
+            int n = arr.Count;
+
+            int key = 57514 | 182339 | 17132 | 773788 | 296934 | 991395 | 303270 | 45231 | 580 | 629822;
+            int key1 = 57514;
+            int key2 = 182339;
+            int key3 = 17132;
+            int key4 = 773788;
+            int key5 = 296934;
+            int key6 = 991395;
+            int key7 = 303270;
+            int key8 = 45231;
+            int key9 = 580;
+            int key10 = 629822;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == key)
+                    return key;
+
+
+            }
+            Console.Write(key1 + "\t" + key2 + "\t" + key3 + "\t" + key4 + "\t" + key5 + "\t"
+                + "\n" + key6 + "\t" + key7 + "\t" + key8 + "\t" + key9 + "\t" + key10 + "\t");
+
+            return -1;
+        }
+        public static int BinarySearch(List<int> arr)
+        {
+            int key = 57514 | 182339 | 17132 | 773788 | 296934 | 991395 | 303270 | 45231 | 580 | 629822;
+            int key1 = 57514;
+            int key2 = 182339;
+            int key3 = 17132;
+            int key4 = 773788;
+            int key5 = 296934;
+            int key6 = 991395;
+            int key7 = 303270;
+            int key8 = 45231;
+            int key9 = 580;
+            int key10 = 629822;
+            int min = 0;
+            int n = arr.Count;
+            int max = n - 1;
+            do
+            {
+                int mid = (min + max) / 2;
+                if (key > arr[mid])
+                    min = mid + 1;
+                else
+                    max = mid - 1;
+                if (arr[mid] == key)
+                    return mid;
+
+             
+
+
+            }
+
+          
+            while (min <= max) ;
+            Console.Write(key1 + "\t" + key2 + "\t" + key3 + "\t" + key4 + "\t" + key5 + "\t"
+            + "\n" + key6 + "\t" + key7 + "\t" + key8 + "\t" + key9 + "\t" + key10 + "\t");
+            return -1;
+
+        }
+    }
+}
+
